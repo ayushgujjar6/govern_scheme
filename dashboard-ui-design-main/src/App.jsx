5ebd525c4ebe7307e6daed052d317cb994542bb3
Ayush Gujjar <ayushprajapati61203@gmail.com>
2025-02-12 16:10:13 +0530

    Added Admin

---
+++ dashboard-ui-design-main/src/App.jsx
@@ -0,0 +1,67 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { ThemeProvider } from "@/contexts/theme-context";
+
+import Layout from "@/routes/layout";
+import DashboardPage from "@/routes/page";
+import Yojana from "@/routes/yojana";
+import NewYojana from "/routes/new-yojana";
+
+function App() {
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <Layout />,
+            children: [
+                {
+                    index: true,
+                    element: <DashboardPage />,
+                },
+                {
+                    path: "analytics",
+                    element: <h1 className="title">Analytics</h1>,
+                },
+                {
+                    path: "reports",
+                    element: <h1 className="title">Reports</h1>,
+                },
+                {
+                    path: "yojana",
+                    element: <Yojana />,
+                },
+                {
+                    path: "new-yojana",
+                    element: <NewYojana />,
+                },
+                {
+                    path: "inventory",
+                    element: <h1 className="title">Inventory</h1>,
+                },
+                {
+                    path: "customers",
+                    element: <h1 className="title">Customers</h1>,
+                },
+                {
+                    path: "new-customer",
+                    element: <h1 className="title">New Customer</h1>,
+                },
+                {
+                    path: "verified-customers",
+                    element: <h1 className="title">Verified Customers</h1>,
+                },
+                {
+                    path: "settings",
+                    element: <h1 className="title">Settings</h1>,
+                },
+            ],
+        },
+    ]);
+
+    return (
+        <ThemeProvider storageKey="theme">
+            <RouterProvider router={router} />
+        </ThemeProvider>
+    );
+}
+
+export default App;
